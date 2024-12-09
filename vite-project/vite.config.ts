b93451e9cc341c93df937b23ab126b5b971aac4d
thwjddlqslek <113258117+thwjddlqslek@users.noreply.github.com>
2024-12-09 23:53:13 +0900

    fix: update vite config for deployment

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // 상대 경로로 변경
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
+});
