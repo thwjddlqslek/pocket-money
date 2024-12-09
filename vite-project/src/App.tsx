45054b0d9c417bf949487505705e5c2223db8f38
thwjddlqslek <113258117+thwjddlqslek@users.noreply.github.com>
2024-12-10 00:10:13 +0900

    fix: App, main 코드 수정

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,27 +1,18 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import store from "./store";
-import ThemeContextProvider from "./context/ThemeContext";
-import GlobalStyle from "./styles/globalStyle";
 import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <ThemeContextProvider>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/main" element={<Main />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeContextProvider>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import GlobalStyle from './styles/globalStyle.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import GlobalStyle from "./styles/globalStyle.tsx";
+import { Provider } from "react-redux";
+import store from "./store";
+import { ThemeContextProvider } from "./context/ThemeContext.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeContextProvider>
+        <GlobalStyle />
+        <App />
+      </ThemeContextProvider>
+    </Provider>
+  </React.StrictMode>
+);
